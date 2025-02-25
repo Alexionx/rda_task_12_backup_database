4ca1cb2c4be45afdd09cee52536ecefa5072af97
KempachiDZ <alexmelnyk48370@gmail.com>
2025-02-25 16:54:20 +0200

    Solve task

--- task.sh
+++ task.sh
@@ -1,1 +1,13 @@
 #! /bin/bash
+
+echo $DB_USER
+echo $DB_PASSWORD
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB > shopdb_backup.sql
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < shopdb_backup.sql
+
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB > shopdb_data_backup.sql
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < shopdb_data_backup.sql
